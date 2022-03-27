05aeeb9955fea3a802178a80593af0d6ffbcb254
belizahrt <belizahrt@gmail.com>
2022-03-27 17:30:41 +0700

    feauture (model): PersonNameValidator init

---
+++ Lab1/Lab1.PersonLib/PersonNameValidator.cs
@@ -0,0 +1,15 @@
+namespace Lab1.PersonLib;
+
+/// <summary>
+/// Person name validator class
+/// </summary>
+public class PersonNameValidator
+{
+    /// <summary>
+    /// Constructor for class
+    /// </summary>
+    public PersonNameValidator()
+    {
+        
+    }
+}
