4d09bb76c68b54832c2bc00dae73b0d1d92d1957
belizahrt <belizahrt@gmail.com>
2022-03-28 15:31:34 +0700

    feauture (model): -enum Locale implemeted -enum PersonNameveValidState implemented

---
+++ Lab1/Lab1.PersonLib/Locale.cs
@@ -0,0 +1,11 @@
+namespace Lab1.PersonLib;
+
+/// <summary>
+/// Locales enum
+/// </summary>
+public enum Locale
+{
+    Undefined,
+    English,
+    Russian
+}
---
+++ Lab1/Lab1.PersonLib/PersonNameValidState.cs
@@ -0,0 +1,11 @@
+namespace Lab1.PersonLib;
+
+/// <summary>
+/// States of person's name invalidation
+/// </summary>
+public enum PersonNameValidState
+{
+    InvalidLocale,
+    InvalidShape,
+    Acceptable
+}
